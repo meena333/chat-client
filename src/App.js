82a972f1635f65c444e44252495fe49cd012fc66
Meenakshi Venkat <meenak.venkat@gmail.com>
2019-08-09 12:26:48 +0200

    Send messages to server, show on page, add form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as request from 'superagent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    message: '',
+    messages: []
+  }
+
+  //manages received events from the server
+  source = new EventSource(`http://localhost:5000/stream`)
+
+  componentDidMount() {
+    this.source.onmessage = (event) => {
+      //console.log('state test: ', this.state)
+
+      //get array from serialized data
+      const messages = JSON.parse(event.data)
+
+      this.setState({ messages })
+    }
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault()
+    console.log('this.state.msg', this.state.message)
+
+    await request.post('http://localhost:5000/message')
+      .send({ message: this.state.message })
+
+    this.setState({ message: '' })
+
+  }
+
+  onChange = (event) => {
+    const { value } = event.target
+    this.setState({ message: value })
+  }
+
+  render() {
+    const messages = this.state.messages.map((message, index) => <p key={index}>
+      {message}
+    </p>)
+
+    const form = <form onSubmit={this.onSubmit}>
+      <input type='text' value={this.state.message} onChange={this.onChange} />
+      <button type='submit'>Send</button>
+    </form>
+    return <main>
+      {form}
+      {messages}
+    </main>
+  }
 }
 
 export default App;
